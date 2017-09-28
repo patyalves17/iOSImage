fd46ecf7b6c78e8227e3a87506370ae15186f8ab
Usuário Convidado <Guest@PA06MICRO14.local>
2017-09-27 21:58:23 -0300

    image and liveRendering

---
+++ LiveRendering/LiveRendering/RoundedImageView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedImageView.swift
+//  LiveRendering
+//
+//  Created by Usuário Convidado on 27/09/17.
+//  Copyright © 2017 Usuário Convidado. All rights reserved.
+//
+
+import UIKit
+
+class RoundedImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
